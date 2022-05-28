4fd830c479a5280187f4f5db0ba0e1c96de598ef
Martin Kostelnik <martinkostelnik0@gmail.com>
2022-05-28 20:08:42 +0200

    Add experiments

---
+++ experiments/experiments.sh
@@ -0,0 +1,5 @@
+bash hopper-default.sh
+bash hopper-gamma0.0.sh
+bash hopper-gamma0.1.sh
+bash hopper-gamma0.5.sh
+bash hopper-gamma0.9.sh
---
+++ experiments/hopper-gamma0.9.sh
@@ -0,0 +1,1 @@
+python ../src/td3.py --env Hopper-v2 --train 200000 --plot-save ../src/model/exp-gamma0.9/plt.pdf --save ../src/model/exp-gamma0.9 --max-env-steps 1000 --policy-delay 2 --warmup 100000 --noise 0.1 --lra 0.001 --lrc 0.001 --tau 0.005 --gamma 0.9
