e78f247b9196a5aafcf58f2674f1f55f76171121
Martin Kostelnik <martinkostelnik0@gmail.com>
2022-05-29 22:19:36 +0200

    Add all experiments and results

---
+++ experiments/hopper-tau1.sh
@@ -0,0 +1,1 @@
+python ../src/td3.py --env Hopper-v2 --train 200000 --plot-save ../src/model/hopper-tau1/plt.pdf --save ../src/model/hopper-tau1 --max-env-steps 1000 --policy-delay 2 --warmup 100000 --noise 0.1 --lra 0.001 --lrc 0.001 --tau 1 --gamma 0.99
