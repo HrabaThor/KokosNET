a6f2224cdfd5d3ddbf14c5148e7d6f00988587cc
Martin Kostelnik <martinkostelnik0@gmail.com>
2022-05-29 22:19:36 +0200

    Add all experiments and results

--- experiments/experiments.sh
+++ experiments/experiments.sh
@@ -1,5 +1,22 @@
 bash hopper-default.sh
+
 bash hopper-gamma0.0.sh
-bash hopper-gamma0.1.sh
 bash hopper-gamma0.5.sh
 bash hopper-gamma0.9.sh
+bash hopper-gamma1.0.sh
+
+bash hopper-nets-64-64-64.sh
+bash hopper-nets-512-256-128.sh
+bash hopper-nets-Aless.sh
+bash hopper-nets-Amore.sh
+
+bash hopper-policy1.sh
+bash hopper-policy4.sh
+bash hopper-policy10.sh
+
+bash hopper-tau0.0.sh
+bash hopper-tau0.1.sh
+bash hopper-tau0.4.sh
+bash hopper-tau0.05.sh
+bash hopper-tau0.8.sh
+bash hopper-tau1.sh
---
+++ experiments/hopper-nets-Aless.sh
@@ -0,0 +1,1 @@
+python ../src/td3.py --env Hopper-v2 --train 200000 --plot-save ../src/model/hopper-Aless/plt.pdf --save ../src/model/hopper-Aless --max-env-steps 1000 --policy-delay 2 --warmup 100000 --noise 0.1 --lra 0.001 --lrc 0.001 --tau 0.005 --gamma 0.99 --actor 256 256 --critic 256 256 256
