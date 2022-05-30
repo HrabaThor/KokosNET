34a0ad1ae95c9f6e9492312c7f2b796809b2f0c2
Michal Szymik <mi.szymik@gmail.com>
2022-05-30 23:20:17 +0200

    fix merge

--- src/compare.py
+++ src/compare.py
@@ -6,7 +6,6 @@ from stable_baselines3.common.results_plotter import load_results, ts2xy
 
 parser = argparse.ArgumentParser()
 
-<<<<<<< HEAD
 parser.add_argument("--env", type=str, 
                     help="Name of environment to compare.")
 parser.add_argument("--r-avg", type=int, default=256,
@@ -20,31 +19,16 @@ parser.add_argument("--save-dir", type=str, default="comparisons",
 # create graphs for report
 parser.add_argument("--compare-all", action='store_true',
                     help="Load weights before training")
-=======
-parser.add_argument("--env", type=str, required=True,
-                    help="Name of environment to compare.")
-parser.add_argument("--r-avg", type=int, default=256,
-                    help="Choose best model according to mean of N last rewards")
-parser.add_argument("--sb3-dir", type=str, required=True,
-                    help="Directory with baseline data")
-parser.add_argument("--dir", type=str, required=True,
-                    help="Directory with our data")
-parser.add_argument("--save-dir", type=str, default="comparisons",
-                    help="Directory to save the plot into")
->>>>>>> 6ddcbdc (update repo)
 
 args = parser.parse_args()
 
 
 
 def get_sb3_data(dir):
-<<<<<<< HEAD
     '''
         Load data from monitor that were collected during the training of stable
         baseline 3 TD3 algorithm.
     '''
-=======
->>>>>>> 6ddcbdc (update repo)
     # load baseline data
     e, r = ts2xy(load_results(dir), 'episodes')
     rewards = np.array(r)
@@ -56,12 +40,9 @@ def get_sb3_data(dir):
     return data
 
 def get_our_data(dir):
-<<<<<<< HEAD
     '''
         Load data that were collected during the training of our algorithm.
     '''
-=======
->>>>>>> 6ddcbdc (update repo)
     # load our data
     data = np.load(dir+"/train_data"+".npz")
     rewards = data["history_reward"]
@@ -69,12 +50,9 @@ def get_our_data(dir):
     return rewards
 
 def plot_rewards(sb3_data, our_data):
-<<<<<<< HEAD
     '''
         Create a graph displaying average rewards from both algorithms.
     '''
-=======
->>>>>>> 6ddcbdc (update repo)
     sns.set_theme()
    
     # plot both data
@@ -88,7 +66,6 @@ def plot_rewards(sb3_data, our_data):
 
     fig.savefig(args.save_dir+"/"+args.env+"_cmp.pdf")
 
-<<<<<<< HEAD
 
 def plot_all():
     '''
@@ -127,9 +104,3 @@ if __name__ == "__main__":
         sb3_data = get_sb3_data(args.sb3_dir)
         our_data = get_our_data(args.dir)
         plot_rewards(sb3_data,our_data)
-=======
-if __name__ == "__main__":
-    sb3_data = get_sb3_data(args.sb3_dir)
-    our_data = get_our_data(args.dir)
-    plot_rewards(sb3_data,our_data)
->>>>>>> 6ddcbdc (update repo)
